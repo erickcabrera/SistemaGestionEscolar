e5a9cf1a47d0843b2e58048d933d94c949f7a4c9
fabs1902 <fabiolamartinez190201@gmail.com>
2020-10-01 22:33:00 -0600

    Diseño frm alumno

--- SistemaEscolar/Formularios/FrmAlumno.Designer.cs
+++ SistemaEscolar/Formularios/FrmAlumno.Designer.cs
@@ -316,7 +316,7 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(589, 530);
+            this.button3.Location = new System.Drawing.Point(577, 530);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(301, 53);
             this.button3.TabIndex = 33;
