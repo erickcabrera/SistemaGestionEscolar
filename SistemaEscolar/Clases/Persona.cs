fc0ea130b61294e42790bbed7a651314a440cb33
Erick Cabrera <cornejoerick7@gmail.com>
2020-10-01 22:08:51 -0600

    Creación de clase profesor, alumno y persona

---
+++ SistemaEscolar/Clases/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEscolar.Clases
+{
+    class Persona
+    {
+        protected string nombres;
+        protected string apellidos;
+        protected string fechaNac;
+        protected string sexo;
+        protected string telefono;
+        protected string direccion;
+        protected string foto;
+
+        public string Nombres { get => nombres; set => nombres = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public string FechaNac { get => fechaNac; set => fechaNac = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Foto { get => foto; set => foto = value; }
+
+        public Persona()
+        {
+
+        }
+        public Persona(string nombres, string apellidos, string fechaNac, string sexo, string telefono, string direccion,string foto)
+        {
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            this.fechaNac = fechaNac;
+            this.sexo = sexo;
+            this.telefono = telefono;
+            this.direccion = direccion;
+            this.foto = foto;
+        }
+        ~Persona()
+        {
+
+        }
+    }
+}
