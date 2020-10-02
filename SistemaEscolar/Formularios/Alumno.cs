3216cd243b9b14f9116f7e3a52836ca360bae45d
fabs1902 <fabiolamartinez190201@gmail.com>
2020-10-01 22:11:03 -0600

    Ventana de gestión de alumnos

---
+++ SistemaEscolar/Formularios/Alumno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using SistemaEscolar.Formularios;
+
+namespace SistemaEscolar.Formularios
+{
+    public partial class Alumno : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+         (
+             int nLeftRect,
+             int nTopRect,
+             int nRightRect,
+             int nBottomRect,
+             int nWidthEllipse,
+             int nHeightEllipse
+         );
+        public Alumno()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void Alumno_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
