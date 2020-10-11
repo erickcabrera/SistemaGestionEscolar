844bb6d1094e33415d0942475a22208f36a473a5
Erick Cabrera <cornejoerick7@gmail.com>
2020-10-11 14:54:56 -0600

    Creación de clase grupo (detalleGradoSeccion)

---
+++ SistemaEscolar/Clases/Grupo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEscolar.Clases
+{
+    class Grupo
+    {
+        private Grado grado;
+        private Seccion seccion;
+        private string turno;
+
+        internal Grado Grado { get => grado; set => grado = value; }
+        internal Seccion Seccion { get => seccion; set => seccion = value; }
+        public string Turno { get => turno; set => turno = value; }
+
+        public Grupo()
+        {
+
+        }
+        public Grupo(Grado grado, Seccion seccion, string turno)
+        {
+            this.grado = grado;
+            this.seccion = seccion;
+            this.turno = turno;
+        }
+        ~Grupo()
+        {
+
+        }
+    }
+}
